965119b8768425a0830bf600e6149b505feb02cb
Alfonso1954 <ricaurtealfonso@gmail.com>
2023-12-03 17:51:57 -0500

    tercer commit:corresponde a la clase 4, se instala npm install react-router-dom y se realiza la conexion de las paginas, levanta bien

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
 import Home from "../Home";
 import Signin from "../Signin";
 import MyAccount from "../MyAccount";
@@ -6,17 +7,25 @@ import MyOrder from "../MyOrder";
 import NotFound from "../NotFound";
 import "./App.css";
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/sign-in", element: <Signin /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/my-order/:id", element: <MyOrder /> },
+    { path: "*", element: <NotFound /> },
+  ]);
+
+  return routes;
+};
+
+const App = () => {
   return (
-    <div className="App" bg="red-200">
-      <Home />
-      <Signin />
-      <MyAccount />
-      <MyOrders />
-      <MyOrder />
-      <NotFound />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
