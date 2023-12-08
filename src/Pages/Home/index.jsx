36476cac85225cbda5a0404108cb3f75dcd3a1c1
Alfonso1954 <ricaurtealfonso@gmail.com>
2023-12-08 16:52:40 -0500

    corresponde a la clase 8:Consumiendo la fakeStore Api para pintar, es importante entender que la Api, es una apliacación externa, que esta en la nube y que se llama a rect

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
   return (
     <Layout>
       Home
-      <Card />
+      <div className="grid grid-cols-4 gap-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card data={item} key={item.id} />
+        ))}
+      </div>
+      {items?.map((item) => (
+        <Card data={item} key={item.id} />
+      ))}
     </Layout>
   );
 }
