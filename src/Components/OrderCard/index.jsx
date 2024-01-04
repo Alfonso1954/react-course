f3fa94f8a0afa96be6a92aacd24281667381e04d
Alfonso1954 <ricaurtealfonso@gmail.com>
2024-01-03 19:21:26 -0500

    SE modifican los archivos index.jsx d e las crapetas Card y Checkout... ubicadas en la carpeta Componentes, cuando hay un producto repetido, se marca un chulito y no deja agregar m{as. Corresponde a la clase 18, funciona bien

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -6,7 +6,7 @@ const OrderCard = (props) => {
   return (
     <div className="flex justify-between items-center mb-3">
       <div className="flex items-center gap-2">
-        <figure className="w-20 h-20">
+        <figure className="w-10 h-10">
           <img
             className="w-full h-full rounded-lg object-cover"
             src={imageUrl}
@@ -15,7 +15,7 @@ const OrderCard = (props) => {
         </figure>
         <p className="text-sm font-light">{title}</p>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-8">
         <p className="text-lg font-medium">{price}</p>
         <XMarkIcon className="h-6 w-6 text-black cursor-pointer"></XMarkIcon>
       </div>
